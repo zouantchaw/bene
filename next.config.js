bea75d6b945504fd9f088748d2d26c8f3914c77e
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-11-03 08:30:29 -0400

    fresh next app

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {
-  experimental: {
-    serverActions: true,
-  },
-  images: {
-    domains: [
-      "public.blob.vercel-storage.com",
-      "res.cloudinary.com",
-      "abs.twimg.com",
-      "pbs.twimg.com",
-      "avatars.githubusercontent.com",
-      "www.google.com",
-      "flag.vercel.app",
-      "illustrations.popsy.co",
-      "lnpzzrkfflhnr9j2.public.blob.vercel-storage.com",
-      "lh3.googleusercontent.com"
-    ],
-  },
-  reactStrictMode: false,
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
