33794ee52d729683897e252538bda9bb35a1bdd9
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-11-12 07:07:15 -0500

    Change dashboard heading and button text to "Orders" and "Create order".

--- app/(dashboard)/dashboard/loading.tsx
+++ app/(dashboard)/dashboard/loading.tsx
@@ -6,8 +6,8 @@ import { Button } from "@/components/ui/button"
 export default function DashboardLoading() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <Button>Fake button</Button>
+      <DashboardHeader heading="Orders" text="Create and manage orders.">
+        <Button>Create order</Button>
       </DashboardHeader>
       <div className="divide-border-200 divide-y rounded-md border">
         <CardSkeleton />
