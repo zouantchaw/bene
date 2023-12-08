826e221c780a3979dab502c87599ea6c68767872
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-12-08 11:03:34 -0500

    Refactor CreateProduct button to use Link instead of Button

--- components/dashboard/inventory/buttons.tsx
+++ components/dashboard/inventory/buttons.tsx
@@ -1,13 +1,17 @@
 'use client';
-import { useSigninModal } from "@/hooks/use-signin-modal";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { useCreateProductModal } from "@/hooks/use-create-product";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 
 export const CreateProduct = ({ variant }) => {
   const productModal = useCreateProductModal();
 
   return (
-    <Button className="px-3" variant="default" size="lg" onClick={productModal.onOpen}>Create Product</Button>
+    // <Button className="px-3" variant="default" size="lg" onClick={productModal.onOpen}>Create Product</Button>
+    <Link href="/dashboard/inventory/create" className={cn(buttonVariants({ size: "default" }))}>
+      Create Product
+    </Link>
   )
 };
