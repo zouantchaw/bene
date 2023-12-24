ec8091947920e0c4e3e6412e815106f4e76e1549
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-12-24 13:26:15 -0500

    Add carousel component to product preview

--- components/dashboard/inventory/product-preview.tsx
+++ components/dashboard/inventory/product-preview.tsx
@@ -10,6 +10,14 @@ import {
   CardFooter,
 } from "@/components/ui/card"
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 interface ProductPreviewProps {
   images: string[]
   name: string
@@ -28,19 +36,36 @@ export function ProductPreview({
   quantity,
 }: ProductPreviewProps) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{name}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <img src={images[0]} alt={name} />
-        <CardDescription>{description}</CardDescription>
+    <Card className="shadow-lg rounded-lg overflow-hidden">
+      <div className="relative">
+        <Carousel>
+          <CarouselPrevious className="absolute z-10 left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2" />
+          <CarouselContent>
+            {images.map((image, index) => (
+              <CarouselItem key={index}>
+                <img src={image} alt={`${name} image ${index + 1}`} className="w-full h-64 object-cover" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext className="absolute z-10 right-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2" />
+        </Carousel>
+      </div>
+      <CardContent className="p-4">
+        <CardHeader className="mb-2">
+          <CardTitle className="text-xl font-bold">{name}</CardTitle>
+        </CardHeader>
+        <CardDescription className="text-gray-700 mb-2">{description}</CardDescription>
+        <div className="flex items-center justify-between">
+          <div className="text-lg font-bold">Price: {price}</div>
+          <div className="text-lg font-bold">Quantity: {quantity}</div>
+        </div>
+        <div className="mt-2">
+          <div className="text-sm font-semibold">Tags:</div>
+          {tags.map((tag, index) => (
+            <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
+          ))}
+        </div>
       </CardContent>
-      <CardFooter>
-        <div>Price: {price}</div>
-        <div>Quantity: {quantity}</div>
-        <div>Tags: {tags.join(', ')}</div>
-      </CardFooter>
     </Card>
   )
 }
