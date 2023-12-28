a63bffd41db2dc13a89548e9f7edf31ac9ab314c
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-12-27 23:10:04 -0500

    Rename inventory create files and add data table component

--- app/(dashboard)/dashboard/inventory/page.tsx
+++ app/(dashboard)/dashboard/inventory/page.tsx
@@ -6,12 +6,30 @@ import { EmptyPlaceholder } from "@/components/shared/empty-placeholder"
 import { DashboardHeader } from "@/components/dashboard/header"
 import { DashboardShell } from "@/components/dashboard/shell"
 import { CreateProduct } from "@/components/dashboard/inventory/buttons"
+import { Product, columns } from "./columns"
+import { DataTable } from "./data-table"
 
 export const metadata = {
   title: "Inventory",
 }
 
+async function getData(): Promise<Product[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      name: "Gold Chair",
+      description: "A luxurious gold chair",
+      tags: ["furniture", "chair", "gold"],
+      price: 500,
+      quantity: 10
+    },
+    // ...
+  ]
+}
+
 export default async function InventoryPage() {
+  const data = await getData()
   const user = await getCurrentUser()
 
   if (!user) {
@@ -24,14 +42,16 @@ export default async function InventoryPage() {
         <CreateProduct variant="default" />
       </DashboardHeader>
       <div>
-        <EmptyPlaceholder>
+        {/* <EmptyPlaceholder>
           <EmptyPlaceholder.Icon name="warehouse" /> 
           <EmptyPlaceholder.Title>No products</EmptyPlaceholder.Title>
           <EmptyPlaceholder.Description>
             You don&apos;t have any products yet. Get started by creating your first product.
           </EmptyPlaceholder.Description>
           <CreateProduct variant="outline" />
-        </EmptyPlaceholder>
+        </EmptyPlaceholder> */}
+        <DataTable columns={columns} data={data} />
+
       </div>
     </DashboardShell>
   )
