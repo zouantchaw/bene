2b1c8cad3e597c0ec6ee82c7187a9d0f70bf5faf
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-12-28 10:32:20 -0500

    Add dropdown menu for actions in inventory columns

--- app/(dashboard)/dashboard/inventory/columns.tsx
+++ app/(dashboard)/dashboard/inventory/columns.tsx
@@ -1,6 +1,17 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -47,5 +58,33 @@ export const columns: ColumnDef<Product>[] = [
   {
     accessorKey: "available",
     header: "Available",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => console.log("Update")}
+            >
+              Update
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="bg-red-500 text-white hover:bg-red-600">
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
