64721fa9394eb414bbe8b02ea4edabfc669ef9cd
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-12-29 16:45:34 -0500

    Add optional id field to ProductSchema

--- lib/validations/product.ts
+++ lib/validations/product.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const ProductSchema = z.object({
+  id: z.string().uuid().optional(),
   name: z.string().min(1, { message: "Please enter a valid name." }).max(255),
   description: z.string().min(10, { message: "Please enter a valid description." }).max(255),
   price: z.coerce.number().gte(0, { message: "Please enter a valid price." }),  
