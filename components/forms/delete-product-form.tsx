35e8af43801f7bbcb24431f9d78d83555a034476
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2024-01-01 17:44:06 -0500

    Delete product form component without unnecessary imports

--- components/forms/delete-product-form.tsx
+++ components/forms/delete-product-form.tsx
@@ -8,16 +8,7 @@ import { useForm } from "react-hook-form"
 import { cn } from "@/lib/utils"
 import { userNameSchema } from "@/lib/validations/user"
 import { buttonVariants } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { toast } from "@/components/ui/use-toast"
 import { Icons } from "@/components/shared/icons"
 
