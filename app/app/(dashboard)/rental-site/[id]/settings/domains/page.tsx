48df7fbfc4b141041fb78f97fa925447152f3b8e
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2024-02-02 11:13:49 -0500

    Update rental site settings domains page

--- app/app/(dashboard)/rental-site/[id]/settings/domains/page.tsx
+++ app/app/(dashboard)/rental-site/[id]/settings/domains/page.tsx
@@ -1,13 +1,13 @@
 import prisma from "@/lib/prisma";
 import Form from "@/components/form";
-import { updateSite } from "@/lib/actions";
+import { updateRentalSite } from "@/lib/actions";
 
 export default async function RentalSiteSettingsDomains({
   params,
 }: {
   params: { id: string };
 }) {
-  const data = await prisma.site.findUnique({
+  const data = await prisma.rentalSite.findUnique({
     where: {
       id: decodeURIComponent(params.id),
     },
@@ -26,7 +26,7 @@ export default async function RentalSiteSettingsDomains({
           placeholder: "subdomain",
           maxLength: 32,
         }}
-        handleSubmit={updateSite}
+        handleSubmit={updateRentalSite}
       />
       <Form
         title="Custom Domain"
@@ -40,7 +40,7 @@ export default async function RentalSiteSettingsDomains({
           maxLength: 64,
           pattern: "^[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}$",
         }}
-        handleSubmit={updateSite}
+        handleSubmit={updateRentalSite}
       />
     </div>
   );
